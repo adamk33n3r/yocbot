2d4684ed7b3f798aaeccba14220e40cbb16cba73
Adam Keenan <adam.g.keenan@gmail.com>
2022-10-06 02:42:49 -0400

    Fix build using NODE_PATH

--- src/CommandLoader.ts
+++ src/CommandLoader.ts
@@ -2,7 +2,13 @@ import { readdirSync } from 'fs';
 import path from 'path';
 
 export class CommandLoader {
+    private get endsWith(): string {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return (process as any)[Symbol.for('ts-node.register.instance')] ? '.ts' : '.js';
+    }
+
     private commandPaths: string[];
+
     constructor(...commandPaths: string[]) {
         this.commandPaths = commandPaths;
     }
@@ -10,7 +16,7 @@ export class CommandLoader {
     public load(fileName: string): void {
         this.commandPaths.flatMap((commandPath) => {
             const commandFiles = readdirSync(path.join(__dirname, commandPath))
-                .filter(file => file === `${fileName}.ts`);
+                .filter(file => file === `${fileName}${this.endsWith}`);
             return commandFiles.map(file => {
                 // eslint-disable-next-line @typescript-eslint/no-var-requires
                 return require(`./${commandPath}/${file}`).default;
@@ -21,7 +27,7 @@ export class CommandLoader {
     public loadAll(): void {
         this.commandPaths.flatMap((commandPath) => {
             const commandFiles = readdirSync(path.join(__dirname, commandPath))
-                .filter(file => file.endsWith('.ts'));
+                .filter(file => file.endsWith(this.endsWith));
             return commandFiles.map(file => {
                 // eslint-disable-next-line @typescript-eslint/no-var-requires
                 return require(`./${commandPath}/${file}`).default;
