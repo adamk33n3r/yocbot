2d77b48120367fbb7103dc8e002439a5d421a4f4
Adam Keenan <adam.g.keenan@gmail.com>
2022-11-03 11:24:48 -0400

    Add live indicator to search

--- src/commands/Music.ts
+++ src/commands/Music.ts
@@ -157,7 +157,7 @@ export abstract class MusicCommands {
                 .setCustomId('search-song-select')
                 .setPlaceholder('Select song to play')
                 .setOptions(results.map((song, idx) => ({
-                    label: `${idx + 1}. ${(song.title || 'No title found')}`,
+                    label: `${idx + 1}. ${song.live ? '🔴 ' : ''}${(song.title || 'No title found')}`,
                     description: song.channel ? `${song.channel.name} ${song.channel.artist ? '🎵' : ''}` : 'No channel name found',
                     value: song.url || '',
                 }))),
